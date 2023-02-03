8d95df88a0d381f655ee754193ccdc6eaf246af2
Abed Nahouli <abednahouli@gmail.com>
2023-02-03 03:48:27 +0200

    added skip command

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const commands = [
       },
     ],
   },
+  {
+    name: "skip",
+    description: "Skips a song!",
+  },
 ];
 
 const rest = new REST({ version: "10" }).setToken(
@@ -113,6 +117,41 @@ client.on("interactionCreate", async (interaction) => {
     return await interaction.followUp({
       content: `⏱️ | Loading track **${track.title}**!`,
     });
+  } else if (interaction.commandName === "skip") {
+    if (!interaction.member.voice.channelId)
+      return await interaction.reply({
+        content: "You are not in a voice channel!",
+        ephemeral: true,
+      });
+    if (
+      interaction.guild.members.me.voice.channelId &&
+      interaction.member.voice.channelId !==
+        interaction.guild.members.me.voice.channelId
+    )
+      return await interaction.reply({
+        content: "You are not in my voice channel!",
+        ephemeral: true,
+      });
+    const query = interaction.options.getString("query");
+    const queue = player.createQueue(interaction.guild, {
+      metadata: {
+        channel: interaction.channel,
+      },
+    });
+
+    // verify vc connection
+    try {
+      if (!queue.connection)
+        await queue.connect(interaction.member.voice.channel);
+    } catch {
+      queue.destroy();
+      return await interaction.reply({
+        content: "Could not join your voice channel!",
+        ephemeral: true,
+      });
+    }
+
+    queue.skip();
   }
 });
 
